d015eb73cd682674dc5d36649b2fcd3209e860be
efekancl <efecelik4207@gmail.com>
2026-01-13 19:36:00 +0300

    Old files deleted, new project has begun.

--- adminpanel/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminpanelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adminpanel'
-
-    def ready(self):
-        import adminpanel.signals
